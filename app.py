70af97b9d79b0305c28264a368eae297d88b50e6
RuthBiney <r.biney@alustudent.com>
2024-07-31 22:51:44 +0200

    Created a new project

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained models
+model_red = joblib.load('model_red.pkl')
+model_white = joblib.load('model_white.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    wine_type = data['wine_type']
+    features = np.array(data['features']).reshape(1, -1)
+
+    if wine_type == 'red':
+        prediction = model_red.predict(features)
+    elif wine_type == 'white':
+        prediction = model_white.predict(features)
+    else:
+        return jsonify({'error': 'Invalid wine type. Use "red" or "white".'})
+
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run()
